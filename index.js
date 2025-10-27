8e30cb3ee6d08211700c533797dd582041256c58
OlofKers <kerssemeeckers.olaf@gmail.com>
2025-10-27 23:01:02 +0100

    Finished the entire app

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
-let passwordDisplay = document.getElementById("password-text")
+let passwordDisplay1 = document.getElementById("password-box1")
+let passwordDisplay2 = document.getElementById("password-box2")
 
 function generatePassword() {
     let password = ""
     for (i = 0; i < 14; i++) {
         password += characters[Math.floor(Math.random() * characters.length)]        
     }
-    passwordDisplay.textContent = "Password: " + password
+    return password    
+}
+
+function displayPassword(){
+    passwordDisplay1.textContent = generatePassword()
+    passwordDisplay2.textContent = generatePassword()
 }
 
 // console.log(generatePassword())
