8b15bb05515aa289b2c083eb0422b1ffa425cadd
OlofKers <kerssemeeckers.olaf@gmail.com>
2025-10-28 22:49:18 +0100

    stretch goal #3 and # 4 , added the abilty to disable numbers and symbols and a lightmode switch

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const allChars =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const noSymbolChars =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const noNumberChars =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
 let passwordDisplay1 = document.getElementById("password-box1");
 let passwordDisplay2 = document.getElementById("password-box2");
-var slider = document.getElementById("password-length");
-var charAmount = document.getElementById("char-amount");
-
+let slider = document.getElementById("password-length");
+let charAmount = document.getElementById("char-amount");
+let noSymbols = false
+let noNumbers = false
 
 slider.oninput = function updateNumbers() {
     charAmount.textContent = "Amount of characters in password: " + this.value;
@@ -12,13 +16,32 @@ slider.oninput = function updateNumbers() {
 
 function generatePassword() {
     let password = "";
-    for (i = 0; i < slider.value; i++) {
-        password += characters[Math.floor(Math.random() * characters.length)];   
-    }
+    if (noSymbols && noNumbers) {
+        for (i = 0; i < slider.value; i++) {
+            password += allChars[Math.floor(Math.random() * 52)];   
+        }
+    } else if (noSymbols) {
+        for (i = 0; i < slider.value; i++) {
+            password += noSymbolChars[Math.floor(Math.random() * noSymbolChars.length)];   
+        }
+    } else if (noNumbers) {
+        for (i = 0; i < slider.value; i++) {
+            password += noNumberChars[Math.floor(Math.random() * noNumberChars.length)];   
+        }
+    } else
+        for (i = 0; i < slider.value; i++) {
+            password += allChars[Math.floor(Math.random() * allChars.length)];   
+        }
     return password;
 }
 
+function toggleNumbers() {
+    noNumbers = !noNumbers;
+}
 
+function toggleSymbols() {
+    noSymbols = !noSymbols;
+}
 
 function displayPassword(){
     passwordDisplay1.textContent = generatePassword();
@@ -28,16 +51,32 @@ function displayPassword(){
 function copyToClipboard(element) {
 
     //store text to copy in a variable.
-    var copyText = element.textContent;
+    let copyText = element.textContent;
 
     //use clipboard API
     navigator.clipboard.writeText(copyText)
 
     //add a new class which increases border size
     element.classList.add('clicked')
-    
+
     //remove new class after 100 milliseconds
     setTimeout(() => element.classList.remove('clicked'), 100)
 }
 
+function toggleLightMode() {
+// Select all relevant elements
+const body = document.querySelector('body');
+const separator = document.querySelector('.seperator');
+const whiteText = document.querySelector('#white-text');
+const content = document.querySelector('#content');
+const slider = document.querySelector('.slider');
+
+// Toggle light mode classes
+body.classList.toggle('lightmode');
+separator?.classList.toggle('lightmode');
+whiteText?.classList.toggle('lightmode');
+content?.classList.toggle('lightmode');
+slider?.classList.toggle('lightmode');
+}
+// body, .seperator, #white-text, #content, .slider
 // console.log(generatePassword())
